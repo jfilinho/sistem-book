2677f95c1c9c164ea707f7a05d940ab69ebadc48
jfilinho <filinhosouza10@gmail.com>
2022-03-30 12:08:25 -0300

    novo commit back

--- config/db.config.js
+++ config/db.config.js
@@ -3,17 +3,16 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     // Não esquecer de criar variável de ambiente com endereço do seu servidor Mongo local em desenvolvimento, e o endereço do cluster do Atlas em produção
-   
-   
+
     const connection = await mongoose.connect(process.env.MONGODB_URI, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
     });
-    console.log("Connected to DB: ", connection.connection.name);
+    console.log("Conectado com o banco de dados: ", connection.connection.name);
   } catch (err) {
-    console.error("Database connection error: ", err);
+    console.error("Erro de conexão com o banco de dados: ", err);
   }
 }
 
